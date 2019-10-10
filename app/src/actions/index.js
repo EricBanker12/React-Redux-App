37c96932ec626628b035d80f5089c5778cc3ce03
ericbanker12 <ericbanker12@gmail.com>
2019-10-10 17:07:57 -0500

    narrow search results

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -7,7 +7,7 @@ export const SEARCH_FAILURE = 'SEARCH_FAILURE'
 export const searchActionCreator = query => dispatch => {
     if (!query) return
     dispatch({type: START_SEARCH})
-    axios.get(`https://api.fda.gov/drug/label.json?search="${query.replace(/\s/g,'+')}"&limit=10`)
+    axios.get(`https://api.fda.gov/drug/label.json?search=openfda.brand_name:"${query.replace(/\s/g,'+')}"+openfda.generic_name:"${query.replace(/\s/g,'+')}"+active_ingredient:"${query.replace(/\s/g,'+')}"+inactive_ingredient:"${query.replace(/\s/g,'+')}"&limit=10`)
         .then(resp => {
             console.log(resp)
             dispatch({type: SEARCH_SUCCESS, payload: resp.data})

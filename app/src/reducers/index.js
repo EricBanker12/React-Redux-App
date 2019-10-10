79b8ca0e3febbdaf33f7c90b749473d0bfe26abc
ericbanker12 <ericbanker12@gmail.com>
2019-10-10 15:24:14 -0500

    create reducer

---
+++ app/src/reducers/index.js
@@ -0,0 +1,15 @@
+const initState = {
+    query: '',
+    loading: false,
+    results: [],
+    error: ''
+}
+
+function reducer(state = initState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer

17325675c3d057e303f81dd0fa55b36a53af3fc0
ericbanker12 <ericbanker12@gmail.com>
2019-10-10 15:30:29 -0500

    create redux store

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducer from './reducers'
+// import SearchForm from './components/SearchForm'
+
 import './App.css';
+
+const state = createStore(reducer, applyMiddleware(thunk))
 
 function App() {
   return (
     <div className="App">
-      
+      <Provider store={state}>
+        {/* <SearchForm /> */}
+      </Provider>
     </div>
   );
 }
